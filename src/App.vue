1931c608829bda94eb4eca5bacb05c499093e604
AF <rzzelenograd@gmail.com>
2020-01-31 00:52:42 +0300

    added axios, basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <!--      <router-link to="/">Home</router-link> |-->
+      <!--      <router-link to="/about">About</router-link>-->
+      <p>Авито.Недвижимость</p>
+      <hr />
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer style="text-align: center">
+      <hr />
+      Alexander Fadeev ©
+    </footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import { store } from "./store.js"
+const axios = require("axios");
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data() {
+    return {
+      property: store.state
     }
+  },
+  mounted() {
+    axios
+      .get("http://134.209.138.34/items")
+      .then(response => store.set(response));
+  }
+};
+</script>
+
+<style lang="scss">
+header {
+  text-align: left;
+  p {
+    margin: 0px 5px;
+    vertical-align: middle;
+    padding-left: 20px;
+  }
+  hr {
+    margin: 5px 0px;
   }
 }
 </style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+export const store = {
+    state: {
+        info: Object
+    },
+    set(newValue) {
+        this.state.info = newValue;
+    }
+}
