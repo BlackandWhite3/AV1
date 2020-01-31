a3d3d2f34106ec37249f4a90f3553c3128da4396
AF <rzzelenograd@gmail.com>
2020-02-01 02:16:30 +0300

    added slider adaptivity

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,23 +2,25 @@
   <main>
     <h2>{{ sale.data[0].title }}</h2>
       <p>{{sale.data[0].price}}</p>
+    <div class="carousel-container">
     <b-carousel
       controls
       indicators
       no-animation="true"
       background="#fff"
-      style="text-shadow: 1px 1px 2px #333; margin: 50px 150px; display: inline-block!important"
+      style="text-shadow: 1px 1px 2px #333; display: inline-block!important"
     >
       <b-carousel-slide v-for="i in sale.data[0].images.length" :key="i">
         <template v-slot:img>
           <img
             class="d-block"
-            style="max-width: 1024px; max-height: 480px"
+            style="max-width: 100%; height: auto;"
             :src="sale.data[0].images[i - 1]"
           />
         </template>
       </b-carousel-slide>
     </b-carousel>
+      </div>
       <p>{{sale.data[0].address}}</p>
       <p>{{sale.data[0].description}}</p>
       <p>{{sale.data[0].sellerName}}</p>
@@ -54,7 +56,12 @@ export default {
 <style scoped>
 main {
   margin: 0 auto;
-  padding: 2% 15%;
+  padding: 2% 0%;
   text-align: center;
 }
+  .carousel-container {
+    max-width: 100%;
+    margin: 0 auto;
+    text-align: center;
+  }
 </style>
