e9526c4bb95bf71d9d76b48c5b7ada5fb10d5a09
AF <rzzelenograd@gmail.com>
2020-01-31 09:27:40 +0300

    added Home.vue layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,27 @@
 <template>
   <main>
-<!--    <div class="item-container" v-for="item in property.info.data" :key="item.id">-->
-<!--      <h3>{{item.title}}</h3>-->
-<!--      <img :src="item.previewImage" >-->
-<!--    </div>-->
-    <b-card bg-variant="light"
-            border-variant="info"
-            v-for="item in property.data" :key="item.id"
-            :img-src="item.previewImage"
-            img-top
-            :title="item.title"
-            :sub-title="item.id.toString()"
+    <b-card
+            class="mycard"
+      bg-variant="light"
+      border-variant="info"
+      v-for="item in property.data"
+      :key="item.id"
+      :img-src="item.previewImage"
+      img-top
+      :title="item.title"
+      :sub-title="item.id.toString()"
     >
       <b-card-text>{{ item.address }}</b-card-text>
-      <b-card-text>{{item.price}}</b-card-text>
+      <b-card-text>{{ item.price }}</b-card-text>
       <b-button variant="primary">
-        <router-link
-              :to="'/about/'+ item.id"
-              tag="a"
-              target="_blank"
-      >About</router-link></b-button>
+        <router-link :to="'/about/' + item.id" tag="a" target="_blank"
+          >About</router-link
+        ></b-button>
     </b-card>
   </main>
 </template>
 
 <script>
-
 export default {
   name: "home",
   props: {
@@ -35,19 +31,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  main {
+main {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: row;
   flex-wrap: wrap;
+    padding: 2% 20%;
 }
-  .item-container {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    min-width: 370px;
-    min-height: 220px;
-    margin: 50px 20px;
-  }
+.item-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  min-width: 370px;
+  min-height: 220px;
+  margin: 50px 20px;
+}
+    .mycard {
+        width: 350px;
+        margin: 50px 20px;
+    }
 </style>
